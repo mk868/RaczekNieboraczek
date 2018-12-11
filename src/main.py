acd4e1d931ec7939290fa65c0db8274c980c6e48
mkucharczyk <maciej.kucharczyk@hotmail.com>
2018-12-11 19:03:20 +0100

    merge

--- src/main.py
+++ src/main.py
@@ -1,6 +1,10 @@
 import sys
 import Configuration
+import xml.etree.ElementTree as ET
 from genetic.Population import Population
+
+tree = ET.parse('configuration.xml')
+root = tree.getroot()
 
 """
 if len(sys.argv) < 2:
@@ -9,17 +13,21 @@ if len(sys.argv) < 2:
     exit(1)
 """
 
-POPULATION_SIZE = 10
-SELECTION_SIZE = 5
+POPULATION_SIZE = int(root.find('population').find('size').text)
+SELECTION_SIZE = int(root.find('selection').find('size').text)
 POCKET_SIZE = 7 #TODO: floor(log2(TSPgenesCount)) + 1
-MUTATION_PROBABILITY = 0.01
+MUTATION_PROBABILITY = float(root.find('mutation').find('propability').text)
+CROSSING_PROBABILITY = float(root.find('crossing').find('propability').text)
+TARGET = float(root.find('target').text)
+ALFA = float(root.find('alfa').text)
+BETA = float(root.find('beta').text)
 
 def checkQuality(data): #mock for TSP rule check
     """
-    compare element data[0] with element data[2] using data[1] method
-    data[0] = gene 1 row
-    data[2] = gene 2 row
-    data[1] = comparison method: '<', '<=', '>', '>=', '==', '!='
+    compare element data[x]['gene1'] with element data[x]['gene2'] using data[x]['method'] method
+    data[x]['gene1'] = gene 1 row
+    data[x]['gene2'] = gene 2 row
+    data[x]['method'] = comparison method: '<', '<=', '>', '>=', '==', '!='
 
     ignore code below
     this example:
